9958664c4ea0665be320c5e4c2941a6b822e29e5
yushizhao <yushizhao@outlook.com>
2017-08-15 18:03:07 +0800

    params related to pow

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -14,41 +14,30 @@ namespace Consensus {
  */
 struct Params {
     uint256 hashGenesisBlock;
-
-    bool fCoinbaseMustBeProtected;
-
-    /** Needs to evenly divide MAX_SUBSIDY to avoid rounding errors. */
-    int nSubsidySlowStartInterval;
-    /**
-     * Shift based on a linear ramp for slow start:
-     *
-     * MAX_SUBSIDY*(t_s/2 + t_r) = MAX_SUBSIDY*t_h  Coin balance
-     *              t_s   + t_r  = t_h + t_c        Block balance
-     *
-     * t_s = nSubsidySlowStartInterval
-     * t_r = number of blocks between end of slow start and first halving
-     * t_h = nSubsidyHalvingInterval
-     * t_c = SubsidySlowStartShift()
-     */
-    int SubsidySlowStartShift() const { return nSubsidySlowStartInterval / 2; }
     int nSubsidyHalvingInterval;
-    int GetLastFoundersRewardBlockHeight() const {
-        return nSubsidyHalvingInterval + SubsidySlowStartShift() - 1;
-    }
+	bool fCoinbaseMustBeProtected;
+	
     /** Used to check majorities for block version upgrade */
     int nMajorityEnforceBlockUpgrade;
     int nMajorityRejectBlockOutdated;
     int nMajorityWindow;
+    /** Block height and hash at which BIP34 becomes active */
+    int BIP34Height;
+    uint256 BIP34Hash;
+    /**
+     * Minimum blocks including miner confirmation of the total of 2016 blocks in a retargetting period,
+     * (nPowTargetTimespan / nPowTargetSpacing) which is also used for BIP9 deployments.
+     * Examples: 1916 for 95%, 1512 for testchains.
+     */
+    uint32_t nRuleChangeActivationThreshold;
+    uint32_t nMinerConfirmationWindow;
     /** Proof of work parameters */
     uint256 powLimit;
     bool fPowAllowMinDifficultyBlocks;
-    int64_t nPowAveragingWindow;
-    int64_t nPowMaxAdjustDown;
-    int64_t nPowMaxAdjustUp;
+    bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
-    int64_t AveragingWindowTimespan() const { return nPowAveragingWindow * nPowTargetSpacing; }
-    int64_t MinActualTimespan() const { return (AveragingWindowTimespan() * (100 - nPowMaxAdjustUp  )) / 100; }
-    int64_t MaxActualTimespan() const { return (AveragingWindowTimespan() * (100 + nPowMaxAdjustDown)) / 100; }
+    int64_t nPowTargetTimespan;
+    int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
 
 	/** Auxpow parameters */
     int16_t nAuxpowChainId;
