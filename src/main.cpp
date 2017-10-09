9d8d8c2d1a30bbf51aa7a02352b28f27d0ed3a4b
yushizhao <yushizhao@outlook.com>
2017-10-09 15:43:37 +0800

    Revert "start blockSubsidy at 50"
    This reverts commit 171356924d4d151ba4a012147c79beb93406bbd4.

--- src/main.cpp
+++ src/main.cpp
@@ -1457,7 +1457,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    CAmount nSubsidy = 12.5 * COIN;
     nSubsidy >>= halvings;
     return nSubsidy;
 }
