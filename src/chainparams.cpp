b9117c099d39939939577238394c257cd7185c79
yushizhao <yushizhao@outlook.com>
2017-11-22 11:54:55 +0800

    classic merged mining

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -60,7 +60,7 @@ public:
         nMaxTipAge = 24 * 60 * 60;
         nPruneAfterHeight = 100000;
 		
-        consensus.AuxpowChainId = 0xffff;
+        consensus.nAuxpowChainId = 0xffff;
         
 		/**
          * Build the genesis block. Note that the output of its generation
