44573ec61a18b05aeeaa756fbf623c963417d585
yushizhao <yushizhao@outlook.com>
2018-04-17 11:25:54 +0800

    missing pow.cpp change

--- src/pow.cpp
+++ src/pow.cpp
@@ -134,3 +134,38 @@ int64_t GetBlockProofEquivalentTime(const CBlockIndex& to, const CBlockIndex& fr
     }
     return sign * r.GetLow64();
 }
+
+bool CheckAuxPowProofOfWork(const CBlockHeader& block, const Consensus::Params& params)
+{
+    if (block.hashPrevBlock.IsNull()) {
+        return true;
+    }
+
+    /* If there is no auxpow, just check the block hash.  */
+    if (!block.auxpow) {
+
+        // if (!CheckProofOfWork(block.GetPoWHash(), block.nBits, params))
+            // return error("%s : non-AUX proof of work failed", __func__);
+
+        return error("non-AUX proof of work is not allowed.");
+    }
+
+    /* We have auxpow.  Check it.  */
+    if (block.nVersion.GetChainId() != params.nAuxpowChainId)
+        return error("%s : block's chain ID is not valid", __func__);
+    
+    if (!block.nVersion.IsAuxpow2()) {
+        if (!block.nVersion.IsAuxpow())
+            return error("%s : No auxpow flag in nVersion", __func__);
+        if (!block.auxpow->check(block.GetHash(), params))
+            return error("%s : AUX POW is not valid", __func__);
+    } else {
+        if (!block.auxpow->check2(block.GetHash(), params))
+            return error("%s : AUX POW 2 is not valid", __func__);
+    }
+    
+    if (!CheckProofOfWork(block.auxpow->getParentBlockPoWHash(), block.nBits, params))
+        return error("%s : AUX proof of work failed", __func__);
+    
+    return true;
+}
